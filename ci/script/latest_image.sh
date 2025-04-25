58513b2606f27b84243235cae30dd27650684b55
gissily <gissily@outlook.com>
2025-04-26 01:25:45 +0800

    fix latest images

---
+++ ci/script/latest_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull ghcr.io/opcal/eclipse-temurin:21-jre
+docker pull nacos/nacos-server:v2.5.1-slim
+docker pull ghcr.io/opcal/sentinel-dashboard:latest
+docker pull hashicorp/consul:latest
+docker pull mysql:8.0
+docker pull apache/seata-server:2.3.0
+docker pull nacos/nacos-server:v2.5.1-slim
+docker pull zookeeper:3.9.3
