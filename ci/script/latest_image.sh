b4f41a9c8b8d9cc218bf61fbfeb46df163d41cc9
gissily <gissily@outlook.com>
2025-10-18 23:30:49 +0800

    development for 2025.0.0.0

--- ci/script/latest_image.sh
+++ ci/script/latest_image.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 docker pull ghcr.io/opcal/eclipse-temurin:21-jre
-docker pull nacos/nacos-server:v2.5.1-slim
+docker pull nacos/nacos-server:v3.1.0-slim
 docker pull ghcr.io/opcal/sentinel-dashboard:latest
 docker pull hashicorp/consul:latest
 docker pull mysql:8.0
-docker pull apache/seata-server:2.3.0
-docker pull nacos/nacos-server:v2.5.1-slim
-docker pull zookeeper:3.9.3
+docker pull apache/seata-server:2.5.0
+docker pull zookeeper:3.9.4
